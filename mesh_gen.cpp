48316647cf6fa554c2990a1c326ca62b653b52f9
edauricio <43607271+edauricio@users.noreply.github.com>
2019-01-24 13:51:55 -0200

    Mesh class ready

--- mesh_gen.cpp
+++ mesh_gen.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  Mesh mesh;
-  mesh.read("malha0.vtk");
-  cout << mesh.x(339, 1) << endl;
+  Mesh mesh("malha0.vtk");
+  //mesh.read("malha0.vtk");
+  cout.precision(6);
+  cout << fixed << mesh.x(30,5) << endl;
+  mesh.set_x(30,5) = 101.24;
+  cout << mesh.x(30,5) << endl;
   mesh.write("teste_malha.vtk");
 
   return 0;
