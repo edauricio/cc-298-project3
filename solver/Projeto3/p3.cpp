56a2beac1dadfb28cb68a2b7a77885ff51f38b3c
edauricio <43607271+edauricio@users.noreply.github.com>
2019-01-28 17:44:01 -0200

    Add files via upload

---
+++ solver/Projeto3/p3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include "p3Header.h"
+#include "Mesh.h"
+
+int main() {
+
+	ExactSol exact;
+	std::cout << exact.theta(1) << "\n"
+						<< p1 << std::endl;
+
+	return 0;
+}
